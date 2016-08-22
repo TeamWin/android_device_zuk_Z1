1e551e101bd9ab2131634d657fdf69962743ac1b
Björn Tantau <github.com@s.bjoern-tantau.de>
2016-08-22 12:48:23 +0200

    Adds decryption support. Needs CM-13.0 sources to compile. I wasn't able to get it to compile with omni. I also changed the device name to ham as Z1 might clash with the Sony Xperia Z1.
    Change-Id: I657e9568e5ea14848b25e15b431e064b4d1b8147

---
+++ extract-files.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+VENDOR=zuk
+DEVICE=ham
+PROPRIETARY_FILES=proprietary-files*.txt
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+for FILE in `cat $PROPRIETARY_FILES | grep -v ^# | grep -v ^$ `; do
+    FILE=$(echo $FILE | sed -e 's|^-||g' | sed -e 's|^+||g')
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    if [ "$#" -eq 1 ]; then
+        cp $1/system/$FILE $BASE/$FILE
+    else
+        adb pull /system/$FILE $BASE/$FILE
+    fi
+done
+
+./setup-makefiles.sh
